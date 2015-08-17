d9c37061477dcb3884aa30095120119032e37f39
Dinesh Kumar <dineshkb@thoughtworks.com>
2015-08-17 15:27:10 +0530

    sending post with python3

--- run_hr_code.py
+++ run_hr_code.py
@@ -1,17 +1,22 @@
 import sublime, sublime_plugin
-import urllib
+import urllib.request
+import urllib.parse
 
 
 class HackerRank:
 	
 	def send_code_to_server(self):
 		# hr_host = "http://www.hackerrank.com"
+		print(sys.version)
 		hr_compile_tests_url = "http://www.hackerrank.com/rest/contests/master/challenges/simple-array-sum/compile_tests"
 		values = {'code': 'print(31)', 'language': 'python3', 'customtestcase': 'false'}
-		data = urllib.parse.urlencode(values)
-		req = urllib.request.Request(hr_compile_tests_url, data.encode('utf-8'))
-		response = urllib.request.urlopen(req)
-		print(response.read())
+		postdata = urllib.parse.urlencode(values)
+		postdata = postdata.encode('utf-8')
+		req = urllib.request.urlopen(hr_compile_tests_url, postdata)
+		# req.add_header("Content-Type","application/x-www-form-urlencoded;charset=utf-8")
+		# print()
+		# response = urllib.request.urlopen(req, data.encode('utf-8'))
+		print(req.read())
 
 class RuncodeCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
