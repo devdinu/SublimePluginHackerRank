7e9d8cbb0c3ab5bb5224e2e6f3a17e4bc4c53a9a
Dinesh Kumar <dineshkb@thoughtworks.com>
2015-08-17 14:56:05 +0530

    making post to compile tests

--- run_hr_code.py
+++ run_hr_code.py
@@ -1,6 +1,19 @@
 import sublime, sublime_plugin
+import urllib
+
+
+class HackerRank:
+	
+	def send_code_to_server(self):
+		# hr_host = "http://www.hackerrank.com"
+		hr_compile_tests_url = "http://www.hackerrank.com/rest/contests/master/challenges/simple-array-sum/compile_tests"
+		values = {'code': 'print(31)', 'language': 'python3', 'customtestcase': 'false'}
+		data = urllib.parse.urlencode(values)
+		req = urllib.request.Request(hr_compile_tests_url, data.encode('utf-8'))
+		response = urllib.request.urlopen(req)
+		print(response.read())
 
 class RuncodeCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		# send_code_to_server()
+		HackerRank().send_code_to_server()
 		self.view.insert(edit, 0, "Submitting code.")
